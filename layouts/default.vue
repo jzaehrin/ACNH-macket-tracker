9d5fc92eecf1fa8db92764f9bc24c6b621e4f158
Célien Heritier <celien.heritier@iomedia.ch>
2020-03-31 20:08:09 +0200

    avec la suite

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
+    <nav class="flex items-center justify-between flex-wrap bg-black p-6">
+      <div class="flex items-center flex-shrink-0 text-white mr-6">
+        <nuxt-link to="/">
+          <img src="~/assets/images/logo.png" alt="" width="250">
+        </nuxt-link>
+      </div>
+      <div class="block lg:hidden">
+        <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+          <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+        </button>
+      </div>
+      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div class="lg:flex-grow">
+          <nuxt-link to="/admin/createuser" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
+            Docs
+          </nuxt-link>
+        </div>
+      </div>
+    </nav>
     <nuxt />
   </div>
 </template>
--- pages/admin/createuser.vue
+++ pages/admin/createuser.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="container-fluid bg-3 text-center">
+  <div class="container-fluid bg-3 text-centers">
     <h1>create user</h1>
     <form method="POST" action="/createUser">
         <label for="admin">Admin id</label>
-        <input name="admin" type="text" value="<%= uuid %>" />
+        <input name="admin" type="text" value="test" />
 
         <input type="submit" value="Send"/>
     </form>
