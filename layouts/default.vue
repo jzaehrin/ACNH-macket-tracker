43e4108391ca2c97faa8047c6dcaadd41568a587
Célien Heritier <celien.heritier@iomedia.ch>
2020-04-01 00:35:53 +0200

    Fix typo

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,7 +18,7 @@
         <v-app-bar-nav-icon
           @click.stop="primaryDrawer.model = !primaryDrawer.model"
         />
-        <v-toolbar-title>Animal Crossing New Stooks</v-toolbar-title>
+        <v-toolbar-title>Animal Crossing New Stonks</v-toolbar-title>
       </v-app-bar>
 
       <v-content>
