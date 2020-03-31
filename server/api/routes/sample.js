c0185299d748a82d0c605b808a3b767296dec91e
Célien Heritier <celien.heritier@iomedia.ch>
2020-03-31 23:13:12 +0200

    Fix mergge

--- server/api/routes/sample.js
+++ server/api/routes/sample.js
@@ -16,7 +16,7 @@ router.post('/', async (req, res) => {
   }
 
   if(user === null) {
-    res.status(403).send({message: "You do not have rights to create a sample"});
+    res.status(403).send("You do not have rights to create a sample");
     return;
   }
 
@@ -25,18 +25,16 @@ router.post('/', async (req, res) => {
   let date = new Date(req.body.date);
   if(req.body.moment === '1')
     date.setUTCHours(12, 0, 0, 0);
-  else
-    date.setUTCHours(0, 0, 0, 0);
 
   try {
-    sample = await db.Sample.create({
+    sample = await Sample.create({
       userId: user.id,
       date: date,
       amount: req.body.amount
     })
-  } catch(error) {
-    if(error.name === 'SequelizeUniqueConstraintError')
-      res.status(403).send({message: "You cannot modify your previous sample"});
+
+    if (sample !== null)
+      res.status(200).send("Sample correctly saved");
     else
       res.status(500).send("Unexpected error");
   } catch(error) {
@@ -44,11 +42,6 @@ router.post('/', async (req, res) => {
       res.status(403).send("You cannot modify your previous sample");
     return
   }
-
-  if (sample !== null)
-    res.status(200).send();
-  else
-    res.status(500).send({message: "Unexpected error"});
 });
 
 module.exports = router;
