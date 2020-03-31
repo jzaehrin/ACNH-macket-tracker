64a06f95bacea8cbd12a0064c9e96e5b8b8d3045
Jonathan Zaehringer <jonathan.zaehringer@gmail.com>
2020-03-31 22:25:37 +0200

    Add identity to generate new uuid

--- server/api/routes/root.js
+++ server/api/routes/root.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
 
 router.post('/valid', function (req, res) {
   db.User.findOne({
@@ -12,6 +13,22 @@ router.post('/valid', function (req, res) {
       res.status(200).send();
     } else {
       res.status(403).send({message: "User not found"});
+    }
+  }).catch(() => {
+    res.status(500).send({message: "Unexpected error"});
+  })
+});
+
+router.get('/identity', function (req, res) {
+  uuid = uuidv4()
+  db.User.create({
+    uuid: uuid,
+    fakeUuid: uuidv4()
+  }).then((user) => {
+    if(user !== null) {
+      res.status(200).send({uuid: user.uuid, fake_uuid: user.fake_uuid});
+    } else {
+      res.status(500).send({message: "Unexpected error"});
     }
   }).catch(() => {
     res.status(500).send({message: "Unexpected error"});
