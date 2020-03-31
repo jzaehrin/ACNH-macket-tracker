dfce109c198387c4094333a8e08db0c5f3b0b2e1
Célien Heritier <celien.heritier@iomedia.ch>
2020-03-31 19:24:54 +0200

    Initial nuxt with tailwind

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container mx-auto px-4">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus vero officiis dolorum similique. Doloribus quibusdam dolorum accusantium saepe quia. Modi vel doloribus placeat explicabo consequatur vitae sunt quasi molestiae eveniet!
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
