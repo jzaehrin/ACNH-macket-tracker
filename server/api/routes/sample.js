c643dcb663ea39934296448ed7fcff1dcf28c9f8
Jonathan Zaehringer <jonathan.zaehringer@gmail.com>
2020-04-01 21:26:17 +0200

    Migrate to momentjs

--- server/api/routes/sample.js
+++ server/api/routes/sample.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const moment = require('moment')
 const router = express.Router()
 
 router.post('/', async (req, res) => {
@@ -16,17 +17,15 @@ router.post('/', async (req, res) => {
   }
 
   if(user === null) {
-    res.status(403).send({message: "You do not have rights to create a sample"});
+    res.status(403).send({message: "You do not have rights to create a sample"})
     return;
   }
 
   let sample
 
-  let date = new Date(req.body.date);
-  if(req.body.moment === '1')
-    date.setUTCHours(12, 0, 0, 0);
-  else
-    date.setUTCHours(0, 0, 0, 0);
+  let date = moment(req.body.date).startOf('day').utc()
+  if(req.body.moment === 1)
+    date.add(12, 'hours')
 
   try {
     sample = await db.Sample.create({
@@ -36,16 +35,16 @@ router.post('/', async (req, res) => {
     })
   } catch(error) {
     if(error.name === 'SequelizeUniqueConstraintError')
-      res.status(403).send({message: "You cannot modify your previous sample"});
+      res.status(403).send({message: "You cannot modify your previous sample"})
     else
-      res.status(500).send({message: "Unexpected error"});
+      res.status(500).send({message: "Unexpected error"})
     return
   }
 
   if (sample !== null)
     res.status(200).send();
   else
-    res.status(500).send({message: "Unexpected error"});
+    res.status(500).send({message: "Unexpected error"})
 });
 
 module.exports = router;
