ba4bd3ad9c9a3903885350822a89c0ca9485e029
Jonathan Zaehringer <jonathan.zaehringer@gmail.com>
2020-04-02 22:18:54 +0200

    Add WeekChart

---
+++ components/sample/weekChart.vue
@@ -0,0 +1,20 @@
+<script>
+  import { Line, mixins } from 'vue-chartjs';
+  export default {
+    extends: Line,
+    mixins: [mixins.reactiveProp],
+    props: ['data'],
+    mounted () {
+      this.renderChart({
+        labels: ['Monday am','Monday pm', 'Tuesday am', 'Tuesday pm', 'Wednesday am', 'Wednesday pm', 'Thursday am', 'Thursday pm', 'Friday am', 'Friday pm', 'Saturday am', 'Saturday pm'],
+        datasets: [
+          {
+            label: 'Market price of the week',
+            backgroundColor: '#f87979',
+            data: this.data
+          }
+        ]
+      })
+    }
+  }
+</script>
